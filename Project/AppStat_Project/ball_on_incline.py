678fc8a0b3cbfdb2b5d0cce9defa0ec5184fab75
Kiril Klein <kleinkiril94@gmail.com>
2020-12-09 18:06:52 +0100

    performed fit

--- Project/AppStat_Project/ball_on_incline.py
+++ Project/AppStat_Project/ball_on_incline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[198]:
+# In[Import]:
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,108 +12,122 @@ import seaborn as sns
 import sys
 from iminuit import Minuit
 from scipy import signal
+from scipy import stats
 
-# In[199]:
+import numpy as np
+from IPython.core.display import Latex
+from sympy import *
+# In[Import custom]:
 basepath = os.path.abspath('')
-workpath = os.path.dirname(basepath)
+dpath = f"{basepath}/Incline"
 from ExternalFunctions import Chi2Regression, BinnedLH, UnbinnedLH
 from ExternalFunctions import nice_string_output, add_text_to_ax    # Useful functions to print fit results on figure
 
-# In[200]:
+# In[Define functions]:
 def weighted_avg(mu,sig):
     """Computes weighted average given mean values and sigma"""
     return np.sum(mu/sig**2)/np.sum(1/sig**2)
+
 def df_arr(data): return np.array(data,dtype='float32')
+
 def quadr(x,a,b,c): return a*x**2+b*x+c
+
 def fast_hist(data, bins ):
     fig, ax = plt.subplots()
     ax.hist(data, bins = bins)
     plt.show()
     
-def step_func(x,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,c1,c2):
-    if x<a1:
-        y = c1
-    elif (x>a1) & (x<a2):
-        y = c2
-    elif (x>a2) & (x<a3):
-        y = c1
-    elif (x>a3) & (x<a4):
-        y = c2
-    elif (x>a4) & (x<a5):
-        y = c1
-    elif (x>a5) & (x<a6):
-        y = c2
-    elif (x>a6) & (x<a7):
-        y = c1
-    elif (x>a7) & (x<a8):
-        y = c2
-    elif (x>a8) & (x<a9):
-        y = c1
-    elif (x>a9) & (x<a10):
-        y = c2
-    else:
-        y = c1
-    return y
-def fit_func(data):
-    x,y = np.array(data[0]['Time (s)']),np.array(data[0]['Channel 1 (V)'])
+def fit_func(x,y,ex = None,ey = None):
+    
     chi2_object = Chi2Regression(step_func,x, y)
     minuit = Minuit(chi2_object, pedantic=False,a1=-.8,a2=-.75 ,a3=-.6,a4=-.55,a5=-.48,
                            a6=-.4,a7=-.35,a8=-.3,a9=-.2,c1=0.,c2=2.5 )
     minuit.migrad();
     return minuit.args
 
-def determine_times(data,index):
-    x, y = x,y = np.array(data[index]['Time (s)']),np.array(data[index]['Channel 1 (V)'])
-    mask0 = y>1 #separate in intervals of peaks and background
-    x0 = x[mask0]
-    y0 = y[mask0]
-    dx0 = np.roll(x0,-1)-x0
-    return dx0
 
-# In[202]:
+def data_to_time(data,index):
+    """returns left boundary, 
+    centre and right boundary of the peaks"""
+    x, y = np.array(data[index]['Time (s)']),np.array(data[index]['Channel 1 (V)'])
+    ind, params = signal.find_peaks(y,height = (2,3),width = (1000,3000))
+    x_c = x[ind]
+    widths = params['widths']
+    ind_l = (ind-widths/2).astype(int)
+    ind_r = (ind+widths/2).astype(int)
+    x_l = x[ind_l]
+    x_r = x[ind_r]
+    return x_l, x_c, x_r
 
+def lprint(*args,**kwargs):
+    """Pretty print arguments as LaTeX using IPython display system 
+    
+    Parameters
+    ----------
+    args : tuple 
+        What to print (in LaTeX math mode)
+    kwargs : dict 
+        optional keywords to pass to `display` 
+    """
+    display(Latex('$$'+' '.join(args)+'$$'),**kwargs)
+    
+def deg_rad(d): return 2*pi*d/360
+    
+def chi2(data, mu):
+    return np.sum((data-mu)**2/mu)
+def minuit_fit(fit_function,x,y,ey):   
+    Ndof_calc = len(x)-3
+    chi2_object = Chi2Regression(fit_function, x, y, ey) 
+    minuit = Minuit(chi2_object, pedantic=False, a=1, b=1,c=1, print_level=0)  
+    minuit.migrad();  # perform the actual fit
+    Chi2_fit = minuit.fval # the chi2 value
+    Prob_fit = stats.chi2.sf(Chi2_fit, Ndof_calc)
+    return minuit, Chi2_fit, Prob_fit
+# In[Error prop]
 
-data = Time1b
-index  = 1
-x, y = np.array(data[index]['Time (s)']),np.array(data[index]['Channel 1 (V)'])
-ind, params = signal.find_peaks(y,height = (2,3),width = (1000,2900))
-x_v = x[ind]
-widths = params['widths']
-ind_l = (ind-widths/2).astype(int)
-ind_r = (ind+widths/2).astype(int)
-x_l = x[ind_l]
-x_r = x[ind_r]
-fig,ax = plt.subplots()
-ax.plot(x,y)
-#ax.set_xlim(-.,-.7)
-ax.vlines(x_v, ymin = 0, ymax = 3, colors = 'k')
-ax.vlines(x_l,ymin = 0, ymax = 3,colors = 'r')
-ax.vlines(x_r,ymin = 0, ymax = 3,colors = 'b')
+# Define variables:
+g,a,theta,dtheta,D,d = symbols("g,a,theta,dtheta,D,d")
+sa,stheta,sdtheta,sD,sd,sg = symbols("sigma_a, sigma_theta, sigma_dtheta, sigma_D, sigma_d, sigma_g")
 
+# Perimeter:
+# Define relation, and print:
+g = a/sin(theta+dtheta)*(1+2/5*D**2/(D**2-d**2))
+lprint(latex(Eq(symbols('g'),g)))
 
-# In[208]:
+# Calculate uncertainty and print:
+sg = sqrt((g.diff(a) * sa)**2 + (g.diff(theta) * stheta)**2 + 
+          (g.diff(dtheta) * sdtheta)**2+ (g.diff(D) * sD)**2+
+          (g.diff(d) * sd)**2)
+lprint(latex(Eq(symbols('sigma_g'), sg)))
 
+# Turn expression into numerical functions 
+fg = sympy.lambdify((a,theta,dtheta,D,d),g, ("math", "mpmath", "sympy"))
+fsg = sympy.lambdify((a,sa,theta,stheta,dtheta,sdtheta,D,sD,d,sd),sg,("math", "mpmath", "sympy"))
 
-path_m1b = f"{basepath}/meas1/big"
-path_m1s = f"{basepath}/meas1/small"
-path_m2b = f"{basepath}/meas2/big"
-path_m2s = f"{basepath}/meas2/small"
-# In[219]:
+# Define values and their errors
+va, vsa = 2,.1
+vtheta, vstheta = deg_rad(30), deg_rad(1) 
+vdtheta,vsdtheta = deg_rad(1), deg_rad(.5)
+vD, vsD = .03, .001
+vd, vsd = .01, .001
+# Numerically evaluate expressions and print 
+vg = fg(va,vtheta,vdtheta,vD,vd)
+vsg = fsg(va,vsa,vtheta,vstheta,vdtheta,vsdtheta,vD,vsD,vd,vsd)
+print(vg)
+print(vsg)
+# In[Specify paths:
+path_m1b = f"{dpath}/meas1b"
+path_m1s = f"{dpath}/meas1s"
+path_m2b = f"{dpath}/meas2b"
+path_m2s = f"{dpath}/meas2s"
 
-
-Time1s[0].shape
-fn_m1b
-
-
-# In[209]:
-
-
+# In[Load times]:
 fn_m1b = [f for f in listdir(path_m1b) if isfile(join(path_m1b, f))]
 fn_m1s = [f for f in listdir(path_m1s) if isfile(join(path_m1s, f))]
 fn_m2b = [f for f in listdir(path_m2b) if isfile(join(path_m2b, f))]
 fn_m2s = [f for f in listdir(path_m2s) if isfile(join(path_m2s, f))]
 Time1b, Time1s, Time2b, Time2s = [],[],[],[]
-Time1bp, Time1sp, Time2bp, Time2sp = [],[],[],[]
+tl_1b, tl_1s, tl_2b, tl_2s = [],[],[],[]
 for i in range(len(fn_m1b)):
     Time1b.append(pd.read_csv(f'{path_m1b}/{fn_m1b[i]}',skiprows = 14 ))
 for i in range(len(fn_m1s)):
@@ -122,56 +136,117 @@ for i in range(len(fn_m2b)):
     Time2b.append(pd.read_csv(f'{path_m2b}/{fn_m2b[i]}',skiprows = 14 ))
 for i in range(len(fn_m2s)):
     Time2s.append(pd.read_csv(f'{path_m2s}/{fn_m2s[i]}',skiprows = 14 ))
-
-
-# In[249]:
-
-
-pd.read_csv(f'{path_m2b}/{fn_m2b[0]}',skiprows = 14 )
-
-
-# In[247]:
-
-
-Time2b[0], fn_m2b[0]
-
-
-# In[217]:
-
+for i in range(len(Time1b)):
+    tl,_,_ = data_to_time(Time1b,i)
+    tl_1b.append(tl)
+for i in range(len(Time1s)):
+    tl,_,_ = data_to_time(Time1s,i)
+    tl_1s.append(tl)
+for i in range(len(Time2b)):
+    tl,_,_ = data_to_time(Time2b,i)
+    tl_2b.append(tl)
+for i in range(len(Time2s)):
+    tl,_,_ = data_to_time(Time2s,i)
+    tl_2s.append(tl)
+tl_1b, tl_1s  =  np.array(tl_1b), np.array(tl_1s)
+tl_2b, tl_2s = np.array(tl_2b), np.array(tl_2s)
+# In[just visualization]:
 
 xlab, ylab = 'Time (s)', 'Channel 1 (V)' 
-files = fn_m1s
+files = fn_m1b
+data = Time1b
+times = tl_1b
+
 fig, ax = plt.subplots(len(files))
 for i in range(len(files)):
-    ax[i].plot(Time1s[i][xlab], Time1s[i][ylab])
-    ax[i].set_xlim((-.9,0.5))
+    ax[i].plot(data[i][xlab], data[i][ylab])
+    ax[i].vlines(times[i],ymin = 0, ymax = 3,colors = 'r')
+    #ax[i].set_xlim((-.8,.7))
 plt.show()
 
+# In[Store data in array]
+data_all = pd.read_csv("project_data.csv", skiprows = 2)
 
-# In[211]:
-
-
-data = pd.read_csv('project_data.csv', sep = ',',skiprows = 2)
-data.head(16)
-
-
-# ## Separate data
-
-# In[212]:
-
-
-mu_angle = df_arr(data.iloc[:15,0])
-s_angle = df_arr(data.iloc[:15,1])
+mu_angle = df_arr(data_all.iloc[:15,0])
+s_angle = df_arr(data_all.iloc[:15,1])
 wa_angle = weighted_avg(mu_angle, s_angle)
-mu_Db = df_arr(data['D_big'][1:5])
-mu_Ds = df_arr(data['D_small'][1:5])
+
+mu_Db = df_arr(data_all['D_big'][1:5])
+mu_Ds = df_arr(data_all['D_small'][1:5])
 s_D = 0.05*np.ones(4)
-mu_drail = df_arr(data['d_rail'][:4])
-s_drail = df_arr(data['d_rail unc'][:4])
-mu_angle_r = df_arr(data['angle rotated'][:15])
-s_angle_r = df_arr(data['angle rot. unc'][:15])
+wa_Db = weighted_avg(mu_Db, s_D)
+wa_Ds = weighted_avg(mu_Ds, s_D)
+
+mu_drail = df_arr(data_all['d_rail'][:4])
+s_drail = df_arr(data_all['d_rail unc'][:4])
+wa_drail = weighted_avg(mu_drail, s_drail)
+
+mu_angle_r = df_arr(data_all['angle rotated'][:15])
+s_angle_r = df_arr(data_all['angle rot. unc'][:15])
+wa_angle_r = weighted_avg(mu_angle_r, s_angle_r)
+
 P_all = []
 for i in range(5):
-    P_all.append(data.iloc[:4,2+i])
-P_all[0]
+    P_all.append(data_all.iloc[:4,2+i])
+P_all = np.array(P_all)
+s_P = .05
+wa_P = []
+P_all = P_all.astype(dtype = 'float32')
+for i in range(5):
+    wa_P.append(weighted_avg(P_all[i],s_P))
+wa_P = np.array(wa_P)
 
+mu_a = df_arr(data_all['a'][1:4])
+mu_b = df_arr(data_all['b'][1:4])
+mu_c = df_arr(data_all['c'][1:4])
+s_abc = np.ones(4)*.05
+diff = np.sqrt(mu_a**2+mu_b**2)-mu_c
+print(diff)
+
+# In[Determine acceleration]
+
+a_1b, sa_1b, args_1b = [],[], []
+a_1s, sa_1s, args_1s = [],[], []
+a_2b, sa_2b, args_2b = [],[], []
+a_2s, sa_2s, args_2s = [],[], []
+for i in range(len(tl_1b)):
+    minuit_obj,Chi2, Prob = minuit_fit(quadr, tl_1b[i],wa_P, s_P )
+    a = minuit_obj.values['a']
+    sa = minuit_obj.errors['a']
+    a_1b.append(a)
+    sa_1b.append(sa)
+    args_1b.append(minuit_obj.args)
+for i in range(len(tl_1s)):
+    minuit_obj,Chi2, Prob = minuit_fit(quadr, tl_1s[i],wa_P, s_P )
+    a = minuit_obj.values['a']
+    sa = minuit_obj.errors['a']
+    a_1s.append(a)
+    sa_1s.append(sa)
+    args_1s.append(minuit_obj.args)
+for i in range(len(tl_2b)):
+    minuit_obj,Chi2, Prob = minuit_fit(quadr, tl_2b[i],wa_P, s_P )
+    a = minuit_obj.values['a']
+    sa = minuit_obj.errors['a']
+    a_2b.append(a)
+    sa_2b.append(sa)
+    args_2b.append(minuit_obj.args)
+for i in range(len(tl_2s)):
+    minuit_obj,Chi2, Prob = minuit_fit(quadr, tl_2s[i],wa_P, s_P )
+    a = minuit_obj.values['a']
+    sa = minuit_obj.errors['a']
+    a_2s.append(a)
+    sa_2s.append(sa)
+    args_2s.append(minuit_obj.args)    
+a_1b,a_1s,a_2b,a_2s = np.array(a_1b), np.array(a_1s), np.array(a_2b), np.array(a_2s)
+fig,ax = plt.subplots()
+ax.plot(tl_2s.T,wa_P,'.')
+x = np.linspace(-.8,0.4,100)
+for i in range(len(tl_2s)):
+    ax.plot(x, quadr(x,*args_2s[i]))
+print(a/100,sa/100)
+
+# In[Compute g]
+va, vsa = ,.1
+vtheta, vstheta = deg_rad(30), deg_rad(1) 
+vdtheta,vsdtheta = deg_rad(1), deg_rad(.5)
+fg(va,vtheta,vdtheta,vD,vd)
